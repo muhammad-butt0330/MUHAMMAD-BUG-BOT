57344fa0627cf3e59e1362038447c2fcdf43c494
muhammad-butt0330 <butttbrandd@gmail.com>
2025-01-03 14:17:28 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,19 +2,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "919074692450"
-global.ownername = "Abhishek Suresh"
+global.ownernomer = "923309899931"
+global.ownername = "MUHAMMAD BIN QASIM"
 global.ytname = "YT: Comedy Melody CH"
 global.socialm = "GitHub: DGXeon"
 global.location = "India, Kerala, Idukki"
 
-global.ownernumber = '919074692450'  //creator number
-global.ownername = 'Abhishek Suresh' //owner name
-global.botname = '𝙰𝙱𝙷𝙸-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
+global.ownernumber = '923309899931'  //creator number
+global.ownername = 'MUHAMMAD BUG BOT' //owner name
+global.botname = 'MUHAMMAD-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
 
 //sticker details
-global.packname = '🎯𝙰𝙱𝙷𝙸-𝙱𝚄𝙶-𝙱𝙾𝚃'
-global.author = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺 𝚂𝚄𝚁𝙴𝚂𝙷🍀'
+global.packname = '🎯MUHAMMAD-𝙱𝚄𝙶-𝙱𝙾𝚃'
+global.author = 'MUHAMMAD BUTT🍀'
 
 //console view/theme
 global.themeemoji = '🎯'
@@ -27,13 +27,13 @@ global.link = 'https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD'
 global.prefa = ['','!','.','#','&']
 
 //false=disable and true=enable
-global.autoRecording = false //auto recording
+global.autoRecording = true //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
-global.autoread_status = false //auto view status/story
+global.autoread_status = true //auto view status/story
 
 //menu type 
 //v1 is image menu, 
@@ -44,11 +44,11 @@ global.typemenu = 'v2'
 
 //reply messages
 global.mess = {
-    done: 'Done✅',
-    prem: 'This feature can be used by premium user only',
-    admin: 'This feature can be used by admin only',
-    botAdmin: 'This feature can only be used when the bot is a group admin ',
-    owner: 'This feature can be used by owner only',
+    done: 'Done MUHAMMAD✅',
+    prem: 'This feature can be used by premium MUHAMMAD user only',
+    admin: 'This feature can be used by MUHAMMAD admin only',
+    botAdmin: 'This feature can only be used when the MUHAMMAD bot is a group admin ',
+    owner: 'This feature can be used by MUHAMMAD owner only',
     group: 'This feature is only for groups',
     private: 'This feature is only for private chats',
     wait: 'Please Wait',    
